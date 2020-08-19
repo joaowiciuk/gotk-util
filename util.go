20cce1c9aa62ff01e552b09a067a5ffbb54b4fd1
joaowiciuk <joaowiciuk@hotmail.com>
2020-08-18 19:35:15 -0500

    Renamed functions

--- util.go
+++ util.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gotk3/gotk3/gtk"
 )
 
-func isWindow(obj glib.IObject) (*gtk.ApplicationWindow, error) {
+func IsWindow(obj glib.IObject) (*gtk.ApplicationWindow, error) {
 	// Make type assertion (as per gtk.go).
 	if win, ok := obj.(*gtk.ApplicationWindow); ok {
 		return win, nil
@@ -15,7 +15,7 @@ func isWindow(obj glib.IObject) (*gtk.ApplicationWindow, error) {
 	return nil, errors.New("not a *gtk.ApplicationWindow")
 }
 
-func isAssistant(obj glib.IObject) (*gtk.Assistant, error) {
+func IsAssistant(obj glib.IObject) (*gtk.Assistant, error) {
 	// Make type assertion (as per gtk.go).
 	if win, ok := obj.(*gtk.Assistant); ok {
 		return win, nil
@@ -23,7 +23,7 @@ func isAssistant(obj glib.IObject) (*gtk.Assistant, error) {
 	return nil, errors.New("not a *gtk.Assistant")
 }
 
-func isDialog(obj glib.IObject) (*gtk.Dialog, error) {
+func IsDialog(obj glib.IObject) (*gtk.Dialog, error) {
 	// Make type assertion (as per gtk.go).
 	if dialog, ok := obj.(*gtk.Dialog); ok {
 		return dialog, nil
@@ -31,7 +31,7 @@ func isDialog(obj glib.IObject) (*gtk.Dialog, error) {
 	return nil, errors.New("not a *gtk.Dialog")
 }
 
-func isEntry(obj glib.IObject) (*gtk.Entry, error) {
+func IsEntry(obj glib.IObject) (*gtk.Entry, error) {
 	// Make type assertion (as per gtk.go).
 	if entry, ok := obj.(*gtk.Entry); ok {
 		return entry, nil
@@ -39,7 +39,7 @@ func isEntry(obj glib.IObject) (*gtk.Entry, error) {
 	return nil, errors.New("not a *gtk.Entry")
 }
 
-func isComboBoxText(obj glib.IObject) (*gtk.ComboBoxText, error) {
+func IsComboBoxText(obj glib.IObject) (*gtk.ComboBoxText, error) {
 	// Make type assertion (as per gtk.go).
 	if comboBoxText, ok := obj.(*gtk.ComboBoxText); ok {
 		return comboBoxText, nil
@@ -47,105 +47,105 @@ func isComboBoxText(obj glib.IObject) (*gtk.ComboBoxText, error) {
 	return nil, errors.New("not a *gtk.Entry")
 }
 
-func isLabel(obj glib.IObject) (*gtk.Label, error) {
+func IsLabel(obj glib.IObject) (*gtk.Label, error) {
 	if label, ok := obj.(*gtk.Label); ok {
 		return label, nil
 	}
 	return nil, errors.New("not a *gtk.Label")
 }
 
-func isSpinButton(obj glib.IObject) (*gtk.SpinButton, error) {
+func IsSpinButton(obj glib.IObject) (*gtk.SpinButton, error) {
 	if spinBtn, ok := obj.(*gtk.SpinButton); ok {
 		return spinBtn, nil
 	}
 	return nil, errors.New("not a *gtk.SpinButton")
 }
 
-func isPopover(obj glib.IObject) (*gtk.Popover, error) {
+func IsPopover(obj glib.IObject) (*gtk.Popover, error) {
 	if popover, ok := obj.(*gtk.Popover); ok {
 		return popover, nil
 	}
 	return nil, errors.New("not a *gtk.Popover")
 }
 
-func isButton(obj glib.IObject) (*gtk.Button, error) {
+func IsButton(obj glib.IObject) (*gtk.Button, error) {
 	if btn, ok := obj.(*gtk.Button); ok {
 		return btn, nil
 	}
 	return nil, errors.New("not a *gtk.Button")
 }
 
-func isSearchEntry(obj glib.IObject) (*gtk.SearchEntry, error) {
+func IsSearchEntry(obj glib.IObject) (*gtk.SearchEntry, error) {
 	if searchEntry, ok := obj.(*gtk.SearchEntry); ok {
 		return searchEntry, nil
 	}
 	return nil, errors.New("not a *gtk.Button")
 }
 
-func isListStore(obj glib.IObject) (*gtk.ListStore, error) {
+func IsListStore(obj glib.IObject) (*gtk.ListStore, error) {
 	if listStore, ok := obj.(*gtk.ListStore); ok {
 		return listStore, nil
 	}
 	return nil, errors.New("not a *gtk.Button")
 }
 
-func isEntryCompletion(obj glib.IObject) (*gtk.EntryCompletion, error) {
+func IsEntryCompletion(obj glib.IObject) (*gtk.EntryCompletion, error) {
 	if entryCompletion, ok := obj.(*gtk.EntryCompletion); ok {
 		return entryCompletion, nil
 	}
 	return nil, errors.New("not a *gtk.Button")
 }
 
-func isSpinner(obj glib.IObject) (*gtk.Spinner, error) {
+func IsSpinner(obj glib.IObject) (*gtk.Spinner, error) {
 	if spinner, ok := obj.(*gtk.Spinner); ok {
 		return spinner, nil
 	}
 	return nil, errors.New("not a *gtk.Spinner")
 }
 
-func isImage(obj glib.IObject) (*gtk.Image, error) {
+func IsImage(obj glib.IObject) (*gtk.Image, error) {
 	if image, ok := obj.(*gtk.Image); ok {
 		return image, nil
 	}
 	return nil, errors.New("not a *gtk.Image")
 }
 
-func isGrid(obj glib.IObject) (*gtk.Grid, error) {
+func IsGrid(obj glib.IObject) (*gtk.Grid, error) {
 	if grid, ok := obj.(*gtk.Grid); ok {
 		return grid, nil
 	}
 	return nil, errors.New("not a *gtk.Grid")
 }
 
-func isTreeView(obj glib.IObject) (*gtk.TreeView, error) {
+func IsTreeView(obj glib.IObject) (*gtk.TreeView, error) {
 	if treeView, ok := obj.(*gtk.TreeView); ok {
 		return treeView, nil
 	}
 	return nil, errors.New("not a *gtk.TreeView")
 }
 
-func isBox(obj glib.IObject) (*gtk.Box, error) {
+func IsBox(obj glib.IObject) (*gtk.Box, error) {
 	if box, ok := obj.(*gtk.Box); ok {
 		return box, nil
 	}
 	return nil, errors.New("not a *gtk.Box")
 }
 
-func isPopoverMenu(obj glib.IObject) (*gtk.PopoverMenu, error) {
+func IsPopoverMenu(obj glib.IObject) (*gtk.PopoverMenu, error) {
 	if popoverMenu, ok := obj.(*gtk.PopoverMenu); ok {
 		return popoverMenu, nil
 	}
 	return nil, errors.New("not a *gtk.PopoverMenu")
 }
 
-func isCellRenderer(obj glib.IObject) (*gtk.CellRenderer, error) {
+func IsCellRenderer(obj glib.IObject) (*gtk.CellRenderer, error) {
 	if cellRenderer, ok := obj.(*gtk.CellRenderer); ok {
 		return cellRenderer, nil
 	}
 	return nil, errors.New("not a *gtk.CellRenderer")
 }
 
-func isCellRendererText(obj glib.IObject) (*gtk.CellRendererText, error) {
+func IsCellRendererText(obj glib.IObject) (*gtk.CellRendererText, error) {
 	if cellRendererText, ok := obj.(*gtk.CellRendererText); ok {
 		return cellRendererText, nil
 	}
